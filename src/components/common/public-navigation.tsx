f85a49b52cdbf0aac14fd58258a998488e40b016
Anamul Hassan <itsanamulhassan@gmail.com>
2025-07-02 03:27:19 +0600

    feat: implement theme provider and context, refactor public navigation, and add section wrapper component

--- src/components/common/public-navigation.tsx
+++ src/components/common/public-navigation.tsx
@@ -1,21 +1,23 @@
 import { Link } from "react-router";
 import { Button } from "../ui/button";
+import SectionWrapper from "./wrapper/section-wrapper";
 
 const PublicNavigation = () => {
   return (
     <nav>
-      <ul className="flex gap-x-4">
-        <li>
-          <Link to="/">
-            <Button variant="outline">Home</Button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/login">
-            <Button variant="secondary">Login</Button>
-          </Link>
-        </li>
-      </ul>
+      <SectionWrapper className="border flex-row justify-between">
+        <Link to="/">
+          <Button variant="outline">Home</Button>
+        </Link>
+        <ul className="flex gap-x-4">
+          <li>
+            <Link to="/login">
+              <Button variant="secondary">Login</Button>
+            </Link>
+          </li>
+          <li></li>
+        </ul>
+      </SectionWrapper>
     </nav>
   );
 };
---
+++ src/components/common/wrapper/section-wrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import type { FC, ReactNode } from "react";
+interface ISectionWrapperProps {
+  children: ReactNode;
+  className?: string;
+}
+const SectionWrapper: FC<ISectionWrapperProps> = ({ children, className }) => {
+  return (
+    <section
+      className={cn(
+        "w-[1300px] flex flex-col items-center  mx-auto my-[150px]",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default SectionWrapper;
